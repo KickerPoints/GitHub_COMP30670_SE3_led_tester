9c73807c73a8d22a1d595c530d6bd3dcc3485186
AdamHaugh <haugha@tcd.ie>
2018-03-16 13:44:46 +0000

    Third Commit: led_tester is a class (shares name with the module) which takes the grid size to be be N from the read in data. Within the led_tester class we define the methods to deal with instructions read in from the data source - if it begins with 'turn on' then we go from 0 to 1, vice-versa with 'turn off' and with 'switch' we just swap the value of the co-ordinates named

--- led_tester/led_tester.py
+++ led_tester/led_tester.py
@@ -1,3 +1,27 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+class led_tester:
+    
+    def __init__(self, N):
+        self.grid_size = N
+        self.grid = [[0 for x in range(self.grid_size)] for y in range(self.grid_size)]
+        
+    def turn_on(self, start_point1, start_point2, end_point1, end_point2):
+        for [i] in range(start_point1, end_point1):
+            for [j] in range (start_point2,end_point2):
+                self.grid[i][j] = 1
+        pass
+    
+    def turn_off(self, start_point1, start_point2, end_point1, end_point2):
+        for [i] in range(start_point1, end_point1):
+            for [j] in range (start_point2,end_point2):
+                self.grid[i][j] = 0
+        pass
+    
+    def switch(self, start_point1, start_point2, end_point1, end_point2):
+        for [i] in range(start_point1, end_point1):
+            for [j] in range (start_point2,end_point2):
+                self.grid[i][j] = not self.grid[i][j]
+        pass
